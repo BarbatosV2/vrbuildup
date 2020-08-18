28c2f25650ab7cb1f9474b3d996b4f92bad3635f
Max Iraci <coolmax230@gmail.com>
2020-08-18 12:40:54 +0930

    Initial Commit
    (how incredibly original is this?)

---
+++ Assets/Scripts/VR/AttachableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class AttachableObject : MonoBehaviour
+{
+    public List<AttachmentPoint> m_attachmentPoints;
+    public bool m_multiAttach = false;
+
+    private void Awake()
+    {
+        foreach(AttachmentPoint p in m_attachmentPoints)
+        {
+            p.m_mainObject = this;
+        }
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
