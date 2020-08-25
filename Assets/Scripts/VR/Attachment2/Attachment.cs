def2ef75b66d2fddc888c3992862c65658f6ce95
Max Iraci <coolmax230@gmail.com>
2020-08-25 14:39:24 +0930

    advanced physics interactions
    -> physics now works with multiple objects
    -> attachments can now be attached anywhere

--- Assets/Scripts/VR/Attachment2/Attachment.cs
+++ Assets/Scripts/VR/Attachment2/Attachment.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class Attachment : MonoBehaviour
 {
     public Collider m_attachmentCollider;
-    public AttachableObject m_mainObject;
+    public List<AttachableObject> m_objects;
     public ConnectionType m_connectionType;
-
-    public ConfigurableJoint m_confJoint;
 
     // Start is called before the first frame update
     void Start()
@@ -16,46 +14,68 @@ public class Attachment : MonoBehaviour
         
     }
 
-    //attaching this to other object
-    private void OnCollisionEnter(Collision collision)
-    {
-        //ensure the other object can actually be attached to
-        AttachableObject _att = collision.transform.GetComponent<AttachableObject>();
-        if(_att != null)
-        {
-            //point of contact between the attachment and the object
-            Vector3 _contactPoint = collision.GetContact(0).point;
+    ////attaching this to other object
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //ensure the other object can actually be attached to
+    //    AttachableObject _att = collision.transform.GetComponent<AttachableObject>();
+    //    if(_att != null && !m_objects.Contains(_att))
+    //    {
+    //        CreateAttachment(_att);
+    //        ////point of contact between the attachment and the object
+    //        //Vector3 _contactPoint = collision.GetContact(0).point;
 
-            //center the attachment onto the attached point (rather than an edge)
-            transform.position = _contactPoint;
+    //        ////center the attachment onto the attached point (rather than an edge)
+    //        //transform.position = _contactPoint;
 
-            //add a FixedJoint between this and the object (not allowed to parent as that screws up taking it off)
-            FixedJoint _j = gameObject.AddComponent<FixedJoint>();
-            _j.connectedBody = _att.GetComponent<Rigidbody>(); 
-            _j.breakForce = 900f;
+    //        ////add a FixedJoint between this and the object (not allowed to parent as that screws up taking it off)
+    //        //FixedJoint _j = gameObject.AddComponent<FixedJoint>();
+    //        //_j.connectedBody = _att.GetComponent<Rigidbody>(); 
+    //        //_j.breakForce = 900f;
 
+    //        //m_objects.Add(_att);
 
-            //set to trigger to prevent any more OnCollisionEnters
-            m_attachmentCollider.isTrigger = true;
-        }
+    //        ////set to trigger to prevent any more OnCollisionEnters
+    //        //m_attachmentCollider.isTrigger = true;
+    //    }
 
-    }
+    //}
+
     //attaching another object to this
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
         //ensure the other object can actually be attached to
         AttachableObject _att = other.transform.GetComponent<AttachableObject>();
-        if(_att != null)
+        if (_att != null && !m_objects.Contains(_att))
         {
-            CreateAttachment(_att);
+            //m_attachmentCollider.isTrigger = true;
+            //point of contact between the attachment and the object
+            Vector3 _contactpoint = other.GetContact(0).point;
+            CreateAttachment(_att, _contactpoint);
+
+            Physics.IgnoreCollision(m_attachmentCollider, other.collider);
+            //transform.GetComponent<Rigidbody>().isKinematic = true;
         }
     }
 
-    void CreateAttachment(AttachableObject otherAtt)
-    {
-        ConfigurableJoint _j = m_mainObject.gameObject.AddComponent<ConfigurableJoint>();
+    ////attaching another object to this
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    print("triggerero");
+    //    //ensure the other object can actually be attached to
+    //    AttachableObject _att = other.transform.GetComponent<AttachableObject>();
+    //    if (_att != null && !m_objects.Contains(_att))
+    //    {
+    //        CreateAttachment(_att);
+    //    }
+    //}
 
-        _j.anchor = transform.localPosition;
+    void CreateAttachment(AttachableObject otherAtt, Vector3 contactPointWorld)
+    {
+        ConfigurableJoint _j = gameObject.AddComponent<ConfigurableJoint>();
+        m_objects.Add(otherAtt);
+
+        _j.anchor = transform.InverseTransformPoint(contactPointWorld)/1.2f;
         _j.connectedBody = otherAtt.GetComponent<Rigidbody>();
 
         //anchor joint to other's position
@@ -63,7 +83,7 @@ public class Attachment : MonoBehaviour
         //_j.axis = Vector3.zero;
 
         //set joint position to attachment point position
-        _j.connectedAnchor = transform.localPosition;
+        _j.connectedAnchor = otherAtt.transform.InverseTransformPoint(contactPointWorld) / 1.2f;
 
         //lock off motion
         _j.xMotion = ConfigurableJointMotion.Locked;
@@ -80,8 +100,11 @@ public class Attachment : MonoBehaviour
 
         //break force
         _j.breakForce = 450f;
+    }
 
-        m_confJoint = _j;
+    private void OnJointBreak(float breakForce)
+    {
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/VR/Interaction/ThrowableSpecial.cs
+++ Assets/Scripts/VR/Interaction/ThrowableSpecial.cs
@@ -7,16 +7,117 @@ using Valve.VR.InteractionSystem;
 public class ThrowableSpecial : MonoBehaviour
 {
     public Hand.AttachmentFlags m_attachmentFlags;
+    private List<Hand> holdingHands = new List<Hand>();
+    private List<Rigidbody> holdingBodies = new List<Rigidbody>();
+    private List<Vector3> holdingPoints = new List<Vector3>();
+
+    private List<Rigidbody> rigidBodies = new List<Rigidbody>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponentsInChildren<Rigidbody>(rigidBodies);
     }
 
     // Update is called once per frame
     void Update()  
     {
-        
+        for (int i = 0; i < holdingHands.Count; i++)
+        {
+            if (holdingHands[i].IsGrabEnding(this.gameObject))
+            {
+                PhysicsDetach(holdingHands[i]);
+            }
+        }
     }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (startingGrabType != GrabTypes.None)
+        {
+            PhysicsAttach(hand, startingGrabType);
+        }
+    }
+
+    //-------------------------------------------------
+    private bool PhysicsDetach(Hand hand)
+    {
+        int i = holdingHands.IndexOf(hand);
+
+        if (i != -1)
+        {
+            holdingHands[i].DetachObject(gameObject, false);
+
+            // Allow the hand to do other things
+            holdingHands[i].HoverUnlock(null);
+
+            // Delete any existing joints from the hand
+            Destroy(holdingHands[i].GetComponent<ConfigurableJoint>());
+
+            Util.FastRemove(holdingHands, i);
+            Util.FastRemove(holdingBodies, i);
+            Util.FastRemove(holdingPoints, i);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    //-------------------------------------------------
+    private void PhysicsAttach(Hand hand, GrabTypes startingGrabType)
+    {
+        PhysicsDetach(hand);
+
+        Rigidbody holdingBody = null;
+        Vector3 holdingPoint = Vector3.zero;
+
+        // The hand should grab onto the nearest rigid body
+        float closestDistance = float.MaxValue;
+        for (int i = 0; i < rigidBodies.Count; i++)
+        {
+            float distance = Vector3.Distance(rigidBodies[i].worldCenterOfMass, hand.transform.position);
+            if (distance < closestDistance)
+            {
+                holdingBody = rigidBodies[i];
+                closestDistance = distance;
+            }
+        }
+
+        // Couldn't grab onto a body
+        if (holdingBody == null)
+            return;
+
+        // Create a configurable joint from the hand to the holding body
+        Rigidbody handRigidbody = Util.FindOrAddComponent<Rigidbody>(hand.gameObject);
+        handRigidbody.isKinematic = true;
+
+        ConfigurableJoint handJoint = hand.gameObject.AddComponent<ConfigurableJoint>();
+        handJoint.connectedBody = holdingBody;
+        handJoint.xMotion = handJoint.yMotion = handJoint.zMotion = ConfigurableJointMotion.Locked;
+
+        ConfigurableJointMotion _m = ConfigurableJointMotion.Locked;
+        ////if something else is already holding this, limit instead of locking the angular motion
+        //if (holdingHands.Count != 0) _m = ConfigurableJointMotion.Locked;
+
+        handJoint.angularXMotion = handJoint.angularYMotion = handJoint.angularZMotion = _m;
+
+        // Don't let the hand interact with other things while it's holding us
+        hand.HoverLock(null);
+
+        // Affix this point
+        Vector3 offset = hand.transform.position - holdingBody.worldCenterOfMass;
+        offset = Mathf.Min(offset.magnitude, 1.0f) * offset.normalized;
+        holdingPoint = holdingBody.transform.InverseTransformPoint(holdingBody.worldCenterOfMass + offset);
+
+        hand.AttachObject(this.gameObject, startingGrabType, m_attachmentFlags);
+
+        // Update holding list
+        holdingHands.Add(hand);
+        holdingBodies.Add(holdingBody);
+        holdingPoints.Add(holdingPoint);
+    }
+
 }
