6940e67fcdb41ef53ae424547206315500cd26ca
woooshh <a1770523@student.adelaide.edu.au>
2020-09-08 15:22:58 +0930

    Worked on radial menu
    aaaa

---
+++ Assets/SteamVR/InteractionSystem/Samples/Scripts/MoveUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class MoveUI : UIElement
+    {
+        public GameObject ui;
+        int position1 = 0;
+        int position2 = 0;
+        int position3 = 0;
+        bool uimove = false;
+        // Start is called before the first frame update
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        protected override void OnButtonClick()
+        {
+            base.OnButtonClick();
+            if (uimove == false)
+            {
+                uimove = true;
+
+                while (ui.transform.position.z <= ui.transform.position.z + 100)
+                {
+                    ui.transform.position += new Vector3(0, 0, 1);
+                }
+                uimove = false;
+            }
+
+        }
+
+
+    }
+}
